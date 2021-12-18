af167303f2be08c7eb8b9fa0f34231915035da08
Srivarshan <srivarshan02@gmail.com>
2021-12-18 22:42:04 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,65 @@
 use rand::Rng;
-use std::io::*;
+// use std::io::*;
 
 // Main function
 fn main() {
     // let (num_clusters, num_feat, num_points, feat) = init();
-    let (num_clusters, num_feat, num_points, feat) = hardcode_init();
-    let cluster_points = init_cluster_points(&num_clusters, &num_points, &feat);
-    dbg!(cluster_points);
+    let (num_clusters, num_points, feat) = hardcode_init();
+    let mut cluster_points = init_cluster_points(&num_clusters, &num_points, &feat);
+    let iter_num = 100;
+    cluster_points = k_means(cluster_points, &feat, iter_num);
+    dbg!(&cluster_points);
 }
 
-// Get the intial data
+fn k_means(cluster_points: Vec<Vec<f32>>, feat: &Vec<Vec<f32>>, iter_num: i32) -> Vec<Vec<f32>> {
+    if iter_num == 0 {
+        return cluster_points;
+    }
+    let mut cluster_new = Vec::<Vec<Vec<f32>>>::new();
+    for _i in 0..cluster_points.len() {
+        cluster_new.push(vec![vec![0.0]]);
+    }
+    for i in 0..feat.len() {
+        let mut min_dist: f32 = get_dist(&cluster_points[0], &feat[i]);
+        let mut min_dist_idx = 0;
+        for j in 0..cluster_points.len() {
+            if get_dist(&cluster_points[j], &feat[i]) < min_dist {
+                min_dist = get_dist(&cluster_points[j], &feat[i]);
+                min_dist_idx = j;
+            }
+        }
+        cluster_new[min_dist_idx].push(feat[i].clone());
+    }
+    for i in 0..cluster_new.len() {
+        cluster_new[i].remove(0);
+    }
+    let mut cluster_points_new = Vec::<Vec<f32>>::new();
+    for _i in 0..cluster_points.len() {
+        cluster_points_new.push(vec![0.0, 0.0]);
+    }
+    for i in 0..cluster_points_new.len() {
+        for j in 0..cluster_points_new[i].len() {
+            let mut sum: f32 = 0 as f32;
+            for k in 0..cluster_new[i].len() {
+                sum += cluster_new[i][k][j];
+            }
+            sum = sum / (cluster_new[i].len() as f32);
+            cluster_points_new[i][j] = sum;
+        }
+    }
+    k_means(cluster_points_new, feat, iter_num-1)
+}
+
+fn get_dist(point1: &Vec<f32>, point2: &Vec<f32>) -> f32 {
+    let mut sum: f32 = 0 as f32;
+    for i in 0..point1.len() {
+        sum += (point1[i] - point2[i]) * (point1[i] - point2[i]);
+    }
+    sum = sum.sqrt();
+    sum
+}
+
+/*
 fn init() -> (i32, i32, i32, Vec<Vec<f32>>) {
     println!("Enter K value (number of clusters) :");
     let num_clusters = input_i32();
@@ -21,11 +71,11 @@ fn init() -> (i32, i32, i32, Vec<Vec<f32>>) {
     let feat = get_feat(&num_feat, &num_points, feat);
     (num_clusters, num_feat, num_points, feat)
 }
+*/
 
-// Hardcode the initial data
-fn hardcode_init() -> (i32, i32, i32, Vec<Vec<f32>>) {
+fn hardcode_init() -> (i32, i32, Vec<Vec<f32>>) {
     let num_points = 10;
-    let num_feat = 2;
+    let _num_feat = 2;
     let num_clusters = 2;
     let feat = vec![
         vec![1.7, 1.8],
@@ -39,10 +89,10 @@ fn hardcode_init() -> (i32, i32, i32, Vec<Vec<f32>>) {
         vec![4.9, 2.7],
         vec![1.3, 4.5],
     ];
-    (num_clusters, num_feat, num_points, feat)
+    (num_clusters, num_points, feat)
 }
 
-// Get features from CLI
+/*
 fn get_feat(num_feat: &i32, num_points: &i32, mut feat: Vec<Vec<f32>>) -> Vec<Vec<f32>> {
     for i in 0..*num_points {
         println!("Enter data point {}", i + 1);
@@ -52,6 +102,7 @@ fn get_feat(num_feat: &i32, num_points: &i32, mut feat: Vec<Vec<f32>>) -> Vec<Ve
     }
     feat
 }
+*/
 
 fn init_cluster_points(
     num_clusters: &i32,
@@ -60,7 +111,7 @@ fn init_cluster_points(
 ) -> Vec<Vec<f32>> {
     let mut rng = rand::thread_rng();
     let mut rand_nums = Vec::<i32>::new();
-    for i in 0..*num_clusters {
+    for _i in 0..*num_clusters {
         let mut num = rng.gen_range(0..*num_points);
         while rand_nums.contains(&num) {
             num = rng.gen_range(0..*num_points);
@@ -76,11 +127,7 @@ fn init_cluster_points(
     cluster_points
 }
 
-fn k_means() {
-    // TODO: Implement clustering algorithm
-}
-
-// Get Integer input
+/*
 fn input_i32() -> i32 {
     let mut input: String = String::new();
     stdin().read_line(&mut input).expect("Read error");
@@ -90,8 +137,9 @@ fn input_i32() -> i32 {
     };
     num
 }
+*/
 
-// Get Float input
+/*
 fn input_f32() -> f32 {
     let mut input: String = String::new();
     stdin().read_line(&mut input).expect("Read error");
@@ -101,3 +149,4 @@ fn input_f32() -> f32 {
     };
     num
 }
+*/
